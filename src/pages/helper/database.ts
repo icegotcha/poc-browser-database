a25c60ae8624f6fa096e7e35898f0aaa92fc300b
Kotchakorn Wongwisutigul <icekotcha@gmail.com>
2024-01-03 22:37:13 +0700

    Add search function

--- src/pages/helper/database.ts
+++ src/pages/helper/database.ts
@@ -17,7 +17,7 @@ export const initDB = (): Promise<IDBDatabase> => {
     request.onupgradeneeded = (event: IDBVersionChangeEvent) => {
       const db = (event.target as IDBOpenDBRequest).result;
       if (!db.objectStoreNames.contains("picture")) {
-        db.createObjectStore("picture");
+        db.createObjectStore("picture", { keyPath: "id" });
       }
     };
   });
@@ -28,8 +28,8 @@ export const addItemsToDatabase = async (
   item: any
 ): Promise<void> => {
   return new Promise((resolve, reject) => {
-    const transaction = db.transaction("gallery", "readwrite");
-    const store = transaction.objectStore("gallery");
+    const transaction = db.transaction("picture", "readwrite");
+    const store = transaction.objectStore("picture");
     transaction.oncomplete = () => {
       resolve();
     };
@@ -43,13 +43,40 @@ export const addItemsToDatabase = async (
   });
 };
 
-export const getItemsFromDatabase = (db: IDBDatabase): Promise<any> => {
+export const getItemsFromDatabase = (db: IDBDatabase): Promise<any[]> => {
   return new Promise((resolve, reject) => {
-    const transaction = db.transaction("gallery", "readonly");
-    const store = transaction.objectStore("gallery");
+    const transaction = db.transaction("picture", "readonly");
+    const store = transaction.objectStore("picture");
     const request = store.getAll();
     request.onsuccess = (event: Event) => {
       resolve((event.target as IDBRequest).result);
+    };
+    request.onerror = (event: Event) => {
+      reject(`IndexedDB request error: ${(event.target as IDBRequest).error}`);
+    };
+  });
+};
+
+export const searchItemsFromDatabase = (
+  db: IDBDatabase,
+  keyword: string
+): Promise<any[]> => {
+  return new Promise((resolve, reject) => {
+    const transaction = db.transaction("picture", "readonly");
+    const store = transaction.objectStore("picture");
+    const request = store.openCursor();
+    const result: any[] = [];
+    request.onsuccess = function (event) {
+      const cursor = (event.target as IDBRequest<IDBCursorWithValue>).result;
+      if (cursor) {
+        const data = cursor.value as any;
+        if (data.name.indexOf(keyword) !== -1) {
+          result.push(data);
+        }
+        cursor.continue();
+      } else {
+        resolve(result);
+      }
     };
     request.onerror = (event: Event) => {
       reject(`IndexedDB request error: ${(event.target as IDBRequest).error}`);
